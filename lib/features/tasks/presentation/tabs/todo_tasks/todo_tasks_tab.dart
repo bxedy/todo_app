1edf8ad96aa310af3bcbdfd28ec174c5c531e3c9
bxedy <joaov.martins2002@gmail.com>
2025-01-05 02:04:25 -0300

    feat: Adiciona as tabs

---
+++ lib/features/tasks/presentation/tabs/todo_tasks/todo_tasks_tab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TodoTasksTab extends StatelessWidget {
+  const TodoTasksTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('TodoTasksTab'));
+  }
+}
