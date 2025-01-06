b3cfd146a0d22a6470a5f92c559d6fb1fccdbcad
bxedy <joaov.martins2002@gmail.com>
2025-01-06 06:14:26 -0300

    feat: Adiciona o controller de tasks

---
+++ lib/features/tasks/presentation/tasks_controller.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/foundation.dart';
+import 'package:todo_app/features/tasks/domain/entities/task_entity.dart';
+import 'package:todo_app/features/tasks/domain/usecases/tasks_usecase.dart';
+import 'package:uuid/uuid.dart';
+
+class TasksController extends ChangeNotifier {
+  final TasksUsecase _tasksUsecase;
+
+  TasksController(this._tasksUsecase);
+
+  List<Task> tasks = <Task>[];
+  bool isLoading = false;
+  int selectedTabIndex = 0;
+
+  Future<void> loadTasks() async {
+    isLoading = true;
+    notifyListeners();
+
+    tasks = await _tasksUsecase.getTasks();
+    notifyListeners();
+  }
+
+  Future<void> createTask(String title, String description) async {
+    await _tasksUsecase.createOrUpdateTask(
+      Task(
+        id: const Uuid().v4(),
+        title: title,
+        description: description,
+        isDone: false,
+      ),
+    );
+    await loadTasks();
+    notifyListeners();
+  }
+
+  Future<void> deleteTask(String id) async {
+    await _tasksUsecase.deleteTask(id);
+    await loadTasks();
+  }
+
+  void updateTab(int index) {
+    selectedTabIndex = index;
+    notifyListeners();
+  }
+}
