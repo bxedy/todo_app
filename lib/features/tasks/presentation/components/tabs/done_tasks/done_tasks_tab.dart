e725a3f5d7c1c3cacde13d7309c7249fe27e0651
bxedy <joaov.martins2002@gmail.com>
2025-01-06 06:14:03 -0300

    refactor: Passa as tabs para a pasta de componentes

---
+++ lib/features/tasks/presentation/components/tabs/done_tasks/done_tasks_tab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DoneTasksTab extends StatelessWidget {
+  const DoneTasksTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('DoneTasksTab'));
+  }
+}
