2316fca7bf2a628ca09e7ee96b4ba3ee955fd73f
bxedy <joaov.martins2002@gmail.com>
2025-01-06 06:13:27 -0300

    feat: Adiciona regra de criação de tasks

--- lib/features/tasks/presentation/components/create_task_bottom_sheet.dart
+++ lib/features/tasks/presentation/components/create_task_bottom_sheet.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/core/ui/app_colors.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:todo_app/features/tasks/presentation/tasks_controller.dart';
 
-class CreateTaskBottomSheet extends StatelessWidget {
-  const CreateTaskBottomSheet({super.key});
+class CreateTaskBottomSheet extends StatefulWidget {
+  const CreateTaskBottomSheet(this.tasksController, {super.key});
+  final TasksController tasksController;
 
-  static void show(BuildContext context) {
+  static void show(BuildContext context, TasksController tasksController) {
     showModalBottomSheet(
       context: context,
       isScrollControlled: true,
-      builder: (_) => const CreateTaskBottomSheet(),
+      builder: (_) => CreateTaskBottomSheet(tasksController),
     );
+  }
+
+  @override
+  CreateTaskBottomSheetState createState() => CreateTaskBottomSheetState();
+}
+
+class CreateTaskBottomSheetState extends State<CreateTaskBottomSheet> {
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController descriptionController = TextEditingController();
+
+  void _createTask() {
+    widget.tasksController
+        .createTask(titleController.text, descriptionController.text);
+
+    Navigator.pop(context);
   }
 
   @override
@@ -22,6 +39,7 @@ class CreateTaskBottomSheet extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           TextField(
+            controller: titleController,
             decoration: InputDecoration(
               hintText: 'What\'s on your mind?',
               hintStyle: GoogleFonts.urbanist(
@@ -29,20 +47,18 @@ class CreateTaskBottomSheet extends StatelessWidget {
                 fontWeight: FontWeight.w500,
                 fontSize: 16,
               ),
-              prefixIcon: SizedBox(
-                width: 24,
-                height: 24,
-                child: Checkbox(
-                  side: const BorderSide(
-                    color: AppColors.mutedAzure,
-                    width: 2,
-                  ),
-                  activeColor: AppColors.blue,
-                  value: false,
-                  shape: RoundedRectangleBorder(
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(12),
+                child: Container(
+                  width: 24,
+                  height: 24,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: AppColors.mutedAzure,
+                      width: 2,
+                    ),
                     borderRadius: BorderRadius.circular(7),
                   ),
-                  onChanged: (value) {},
                 ),
               ),
               border: InputBorder.none,
@@ -51,6 +67,7 @@ class CreateTaskBottomSheet extends StatelessWidget {
           ),
           const SizedBox(height: 10),
           TextField(
+            controller: descriptionController,
             decoration: InputDecoration(
               hintText: 'Add a note...',
               hintStyle: GoogleFonts.urbanist(
@@ -68,22 +85,36 @@ class CreateTaskBottomSheet extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 20),
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text(
-              'Create',
-              style: GoogleFonts.urbanist(
-                color: AppColors.blue,
-                fontWeight: FontWeight.w700,
-                fontSize: 16,
-              ),
-            ),
-          ),
+          ValueListenableBuilder(
+              valueListenable: titleController,
+              builder: (context, __, _) {
+                return AnimatedOpacity(
+                  opacity: titleController.text.isNotEmpty ? 1 : .2,
+                  duration: const Duration(milliseconds: 300),
+                  child: TextButton(
+                    onPressed:
+                        titleController.text.isNotEmpty ? _createTask : null,
+                    child: Text(
+                      'Create',
+                      style: GoogleFonts.urbanist(
+                        color: AppColors.blue,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                );
+              }),
           SizedBox(height: MediaQuery.of(context).viewInsets.bottom),
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    descriptionController.dispose();
+    super.dispose();
   }
 }
