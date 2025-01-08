8d4599611d8ffc79f9a3aa86cde84152a74a299e
bxedy <86973277+bxedy@users.noreply.github.com>
2025-01-08 08:34:47 -0300

    feat: Torna o campo de texto de descrição multilinhas

--- lib/features/tasks/presentation/components/create_task_bottom_sheet.dart
+++ lib/features/tasks/presentation/components/create_task_bottom_sheet.dart
@@ -50,73 +50,80 @@ class CreateTaskBottomSheetState extends State<CreateTaskBottomSheet> {
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
-          TextField(
-            controller: titleController,
-            decoration: InputDecoration(
-              hintText: 'What\'s on your mind?',
-              hintStyle: GoogleFonts.urbanist(
-                color: AppColors.mutedAzure,
-                fontWeight: FontWeight.w500,
-                fontSize: 16,
-              ),
-              prefixIcon: Padding(
-                padding: const EdgeInsets.all(12),
-                child: Container(
-                  width: 24,
-                  height: 24,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: AppColors.mutedAzure,
-                      width: 2,
+          Flexible(
+            child: TextField(
+              controller: titleController,
+              decoration: InputDecoration(
+                hintText: 'What\'s on your mind?',
+                hintStyle: GoogleFonts.urbanist(
+                  color: AppColors.mutedAzure,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16,
+                ),
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Container(
+                    width: 24,
+                    height: 24,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: AppColors.mutedAzure,
+                        width: 2,
+                      ),
+                      borderRadius: BorderRadius.circular(7),
                     ),
-                    borderRadius: BorderRadius.circular(7),
                   ),
                 ),
+                border: InputBorder.none,
+                contentPadding: const EdgeInsets.symmetric(vertical: 10.0),
               ),
-              border: InputBorder.none,
-              contentPadding: const EdgeInsets.symmetric(vertical: 10.0),
             ),
           ),
           const SizedBox(height: 10),
-          TextField(
-            controller: descriptionController,
-            decoration: InputDecoration(
-              hintText: 'Add a note...',
-              hintStyle: GoogleFonts.urbanist(
-                color: AppColors.mutedAzure,
-                fontWeight: FontWeight.w500,
-                fontSize: 16,
+          Flexible(
+            child: TextField(
+              controller: descriptionController,
+              maxLines: 100,
+              minLines: 1,
+              decoration: InputDecoration(
+                hintText: 'Add a note...',
+                hintStyle: GoogleFonts.urbanist(
+                  color: AppColors.mutedAzure,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16,
+                ),
+                prefixIcon: const Icon(
+                  Icons.edit,
+                  color: AppColors.mutedAzure,
+                  size: 24,
+                ),
+                border: InputBorder.none,
+                contentPadding: const EdgeInsets.symmetric(vertical: 10.0),
               ),
-              prefixIcon: const Icon(
-                Icons.edit,
-                color: AppColors.mutedAzure,
-                size: 24,
-              ),
-              border: InputBorder.none,
-              contentPadding: const EdgeInsets.symmetric(vertical: 10.0),
             ),
           ),
           const SizedBox(height: 20),
           ValueListenableBuilder(
-              valueListenable: titleController,
-              builder: (context, __, _) {
-                return AnimatedOpacity(
-                  opacity: titleController.text.isNotEmpty ? 1 : .2,
-                  duration: const Duration(milliseconds: 300),
-                  child: TextButton(
-                    onPressed:
-                        titleController.text.isNotEmpty ? createTask : null,
-                    child: Text(
-                      'Create',
-                      style: GoogleFonts.urbanist(
-                        color: AppColors.blue,
-                        fontWeight: FontWeight.w700,
-                        fontSize: 16,
-                      ),
+            valueListenable: titleController,
+            builder: (context, __, _) {
+              return AnimatedOpacity(
+                opacity: titleController.text.isNotEmpty ? 1 : .2,
+                duration: const Duration(milliseconds: 300),
+                child: TextButton(
+                  onPressed:
+                      titleController.text.isNotEmpty ? createTask : null,
+                  child: Text(
+                    'Create',
+                    style: GoogleFonts.urbanist(
+                      color: AppColors.blue,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 16,
                     ),
                   ),
-                );
-              }),
+                ),
+              );
+            },
+          ),
           SizedBox(height: MediaQuery.of(context).viewInsets.bottom),
         ],
       ),
