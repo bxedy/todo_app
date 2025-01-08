8d28f067b4baa7b1a9df5aa657b3ac885d3bc53d
bxedy <86973277+bxedy@users.noreply.github.com>
2025-01-08 09:07:56 -0300

    feat: Muda e adiciona métodos do usecase

--- lib/features/tasks/domain/usecases/tasks_usecase.dart
+++ lib/features/tasks/domain/usecases/tasks_usecase.dart
@@ -1,21 +1,19 @@
-import 'package:todo_app/features/tasks/data/repository/local/tasks_repository.dart';
+import 'package:todo_app/features/tasks/data/repository/local/tasks_local_repository.dart';
 import 'package:todo_app/features/tasks/domain/entities/task_entity.dart';
+import 'package:uuid/uuid.dart';
 
 abstract class TasksUsecase {
-  Future<int> createOrUpdateTask(Task task);
+  Future<int> createTask(String title, String description);
+  Future<int> updateTaskToDone(Task task);
   Future<int> deleteTask(String id);
-  Future<List<Task>> getTasks();
+  Future<int> deleteAllDoneTasks();
+  Future<List<Task>> getTasks({required bool isDone, String? query});
 }
 
 class TasksUsecaseImpl implements TasksUsecase {
-  final TasksRepository _repository;
+  final TasksLocalRepository _repository;
 
   TasksUsecaseImpl(this._repository);
-
-  @override
-  Future<int> createOrUpdateTask(Task task) {
-    return _repository.createOrUpdateTask(task);
-  }
 
   @override
   Future<int> deleteTask(String id) {
@@ -23,7 +21,34 @@ class TasksUsecaseImpl implements TasksUsecase {
   }
 
   @override
-  Future<List<Task>> getTasks() {
-    return _repository.getTasks();
+  Future<List<Task>> getTasks({bool? isDone, String? query}) async {
+    if (query?.isNotEmpty == true) {
+      return _repository.searchTasksByTitle(query!);
+    }
+    return isDone == true
+        ? _repository.getTasksDone()
+        : _repository.getTodoTasks();
+  }
+
+  @override
+  Future<int> createTask(String title, String description) {
+    return _repository.createOrUpdateTask(
+      Task(
+        id: const Uuid().v4(),
+        title: title,
+        description: description,
+        isDone: false,
+      ),
+    );
+  }
+
+  @override
+  Future<int> updateTaskToDone(Task task) {
+    return _repository.createOrUpdateTask(task);
+  }
+
+  @override
+  Future<int> deleteAllDoneTasks() {
+    return _repository.deleteAllDoneTasks();
   }
 }
